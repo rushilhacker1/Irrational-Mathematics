66a26dd36e3f3ea1ea21d68c465573bad22d85ad
rushil <85025732+rushilhacker1@users.noreply.github.com>
2023-11-16 08:57:14 +0530

    a

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Irrational Mathematics
+
+Brief description of your website.
+
+## Table of Contents
+
+- [About](#about)
+- [Features](#features)
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## About
+
+Provide a concise overview of your website. Mention its purpose, key features, and any other relevant information.
+
+## Features
+
+- List some of the main features of your website.
+- ...
+
+## Getting Started
+
+### Prerequisites
+
+- Specify any prerequisites that users need to install or set up before using your website.
+- ...
+
+### Installation
+
+1. Clone the repository.
+   ```bash
+   git clone https://github.com/your-username/your-repository.git
+
  
--- app.js
+++ app.js
@@ -1,44 +1,96 @@
-const fs = require("fs")
-const https = require("https")
-const port = 3000
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const https = require("https");
+const port = 3000;
+const logFilePath = "server.log";
 
-const https_certificates = {
-  key: fs.readFileSync('key.pem'), // Update with your actual private key path
-  cert: fs.readFileSync('cert.pem'), // Update with your actual certificate path
+const httpsCertificates = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
 };
 
-const server = https.createServer( https_certificates,
-    function(req, res){
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        const userAgent = req.headers['user-agent'];
-        if (userAgent && userAgent.toLowerCase().includes('mobile')){
-            fs.readFile('b.htm', function(error, data){
-                if (error) {
-                    res.writeHead(404)
-                    res.write('Error: File Not Found')
-                } else {
-                    res.write(data)
-                }
-                res.end()
-            })
-        } else {
-            fs.readFile('a.htm', function(error, data){
-                if (error) {
-                    res.writeHead(404)
-                    res.write('Error: File Not Found')
-                } else {
-                    res.write(data)
-                }
-                res.end()
-            })
-        }
-    }
-)
+const constantsDirectory = "constants";
+const dataInRam = {};
 
-server.listen(port, function(error){
-    if(error) {
-        console.log('Something went wrong, ' + error)
+function readHtmlFile(filePath, res) {
+  fs.readFile(filePath, (error, data) => {
+    if (error) {
+      res.writeHead(404);
+      res.write("Error: File Not Found");
+      logError(`Error reading file: ${filePath}`);
     } else {
-        console.log("Server is listening on port " + port);
+      res.write(data);
+      logInfo(`File ${filePath} served successfully`);
     }
-})
+    res.end();
+  });
+}
+
+function logInfo(message) {
+  const logEntry = `[INFO] ${getCurrentDateTime()} - ${message}\n`;
+  fs.appendFile(logFilePath, logEntry, (err) => {
+    if (err) console.error(`Error writing to log file: ${err}`);
+  });
+}
+
+function logError(message) {
+  const logEntry = `[ERROR] ${getCurrentDateTime()} - ${message}\n`;
+  fs.appendFile(logFilePath, logEntry, (err) => {
+    if (err) console.error(`Error writing to log file: ${err}`);
+  });
+}
+
+function getCurrentDateTime() {
+  const now = new Date();
+  return now.toISOString();
+}
+
+// Read data from files in /constants directory and store it in RAM on server startup
+fs.readdirSync(constantsDirectory).forEach((fileName) => {
+  const filePath = `${constantsDirectory}/${fileName}`;
+  const fileData = fs.readFileSync(filePath, "utf8");
+  dataInRam[fileName] = fileData;
+});
+
+app.get("/names", (req, res) => {
+  const fileNames = fs.readdirSync(constantsDirectory);
+  res.json({ names: fileNames });
+});
+
+app.get("/data/:fileName", (req, res) => {
+  const fileName = req.params.fileName;
+  const filePath = `${constantsDirectory}/${fileName}`;
+
+  if (dataInRam[fileName]) {
+    // If data is in RAM, serve it from there
+    res.json({ fileName, data: dataInRam[fileName] });
+  } else {
+    // If data is not in RAM, read it from the file and store in RAM
+    try {
+      const fileData = fs.readFileSync(filePath, "utf8");
+      dataInRam[fileName] = fileData;
+      res.json({ fileName, data: fileData });
+    } catch (error) {
+      res.status(404).json({ error: "File not found" });
+    }
+  }
+});
+
+app.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  const userAgent = req.headers["user-agent"];
+
+  if (userAgent && userAgent.toLowerCase().includes("mobile")) {
+    readHtmlFile("home.html", res);
+  } else {
+    readHtmlFile("home.html", res);
+  }
+});
+
+const server = https.createServer(httpsCertificates, app);
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+  logInfo("Server started successfully");
+});
